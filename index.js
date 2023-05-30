4e22d551bd3e88292405c5c9639758e6dcc566f3
saurabh pal <131267391+saurabhsigna@users.noreply.github.com>
2023-05-30 04:25:53 +0000

    added dynamic port by .env

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.use("/", localRoutes);
 app.use("/auth", authRoutes);
 app.use("/", testingRoutes);
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
 });
