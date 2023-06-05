d6f6ffe6390433ed57ec02e48c3f0f91cdc8db58
saurabhsigna <131267391+saurabhsigna@users.noreply.github.com>
2023-06-05 05:31:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("testing 6 ");
+  res.send("testing 7 ");
 });
 
 // Profile page (protected route)
